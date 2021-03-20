c3bd23bcedc0461db4b2dd03e6342bee1215423d
Ashly1999 <74659110+Ashly1999@users.noreply.github.com>
2021-03-20 10:20:46 +0530

    Add files via upload

---
+++ book.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Book details</title>
+</head>
+<body><center>
+<h1>Enter the book Details</h1></center>
+<form method=post action="book_details.php">
+<table border=0 align =center cellspacing=5 cellpadding=5>
+<tr><td>Title</td><td><input type="text" name=txt1></td></tr>
+<tr><td>Author</td><td><input type="text" name=txt2></td></tr>
+<tr><td>Edition</td><td><input type="text" name=txt3></td></tr>
+<tr><td>Publisher</td><td><input type="text" name=txt4></td></tr>
+<tr><td><input type=submit value=Submit></td><td><input type=reset value=Reset></td></tr>
+</table>
+</form>
+</body>
