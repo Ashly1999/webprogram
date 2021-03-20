35457b9a31581a4773b891f3c545cc8bbccb5013
Ashly1999 <74659110+Ashly1999@users.noreply.github.com>
2021-03-20 10:11:08 +0530

    Add files via upload

---
+++ C6.1.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>home page</title>
+</head>
+<body bgcolor="linen">
+<br><br>
+<center><h1>Book Details</h1>
+<table border=0 cellspacing=10 cellpadding=50>
+<tr><td><a href=book.php>ADD BOOK DETAILS</a></td><td> <a href=showbook.php>SHOW BOOK DETAILS</a> </td></tr>  
+</table>
+<form action="book_search.php" method="get">
+<table border=0 cellpadding=20>
+<tr><td><h3>Search Book</h3></td><td><input type=text name=serach></td>
+<td><input type=submit value=SEARCH>
+</tr></table></form>
+</center>
+</body>
+</html>   
